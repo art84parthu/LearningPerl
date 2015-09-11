2644fe67a779f5d7409b1451b01c34f70690a49b
Arthy Vijayaraghavan <arthy.vijayaraghavan@booking.com>
2015-09-11 14:55:04 +0200

    modified and tried again

--- optest.pl
+++ optest.pl
@@ -1,11 +1,14 @@
 #!/usr/bin/perl
-$a = 10;
-$b = 100;
-print "$a " . $a . "\n";
-print "$b " . $b . "\n";
-$a //= $b;
-print "$a " . $a . "\n";
+$a;
+$b;
+print "a = " . $a . "\n";
+print "b = " . $b . "\n";
+$a //= 10;
+print "a after slanting or = " . $a . "\n";
+$b ||= 10;
+print "b after straight or = " . $b . "\n";
 
-$b //= $a;
-print "$b " . $b . "\n";
-
+$a //= 100;
+$b ||= 100;
+print "a after slanting or = " . $a . "\n";
+print "b after straight or = " . $b . "\n";
